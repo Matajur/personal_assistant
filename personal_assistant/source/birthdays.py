5356b8c45ff80c8a700eefb1dc340944b5c93be5
omelnychenko <itsfpaS$c0de1>
2024-03-15 18:38:58 +0200

    Added upcoming birthdays

--- personal_assistant/source/birthdays.py
+++ personal_assistant/source/birthdays.py
@@ -17,19 +17,28 @@ def search_upcoming_birthday_contacts(book: AddressBook, *_) -> None:
         print(f"|{' ' * COLUMN_1}|{'Contact book is empty':<{FIELD}}|")
         return
 
+    handle_contacts(book)
+
+
+def handle_contacts(book: AddressBook) -> None:
     days = get_days()
     today = datetime.now().date()
     end_date = today + timedelta(days=days)
-    contacts = []
+    contacts = get_contacts(book, today, end_date)
 
-    for contact in book.values():
-        if contact.birthday is not None:
-            origin_birthday = contact.birthday.value
-            this_year_birthday = datetime(year=today.year, month=origin_birthday.month, day=origin_birthday.day).date()
-            if today <= this_year_birthday <= end_date:
-                contacts.append(contact)
-
-    show_result(today, end_date, contacts)
+    if len(contacts):
+        today_formatted = today.strftime('%d.%m.%Y')
+        end_date_formatted = end_date.strftime('%d.%m.%Y')
+        print(SEPARATOR)
+        print(f"|{INDENT}|{'Birthdays in range ' + today_formatted + ' - ' + end_date_formatted:<{FIELD}}|")
+        print(SEPARATOR)
+        print(HEADER)
+        print(SEPARATOR)
+        for number, record in enumerate(contacts):
+            print(f"|{number + 1:^{COLUMN_1}}|{record}|")
+    else:
+        print(SEPARATOR)
+        print(f"|{' ' * COLUMN_1}|{'Empty result':<{FIELD}}|")
 
 
 def get_days() -> int:
@@ -43,16 +52,12 @@ def get_days() -> int:
         print(f"|{INDENT}|{'The number must be of integer type only':<{FIELD}}|")
 
 
-def show_result(start_day: date, end_day: date, result: []) -> None:
-    if len(result):
-        date_range = start_day.strftime('%d.%m.%Y') + ' - ' + end_day.strftime('%d.%m.%Y')
-        print(SEPARATOR)
-        print(f"|{INDENT}|{'Birthdays in range ' + date_range:<{FIELD}}|")
-        print(SEPARATOR)
-        print(HEADER)
-        print(SEPARATOR)
-        for number, record in enumerate(result):
-            print(f"|{number + 1:^{COLUMN_1}}|{record}|")
-    else:
-        print(SEPARATOR)
-        print(f"|{' ' * COLUMN_1}|{'Empty result':<{FIELD}}|")
+def get_contacts(book: AddressBook, today: date, end_date: date) -> []:
+    contacts = []
+    for contact in book.values():
+        if contact.birthday is not None:
+            origin_birthday = contact.birthday.value
+            this_year_birthday = datetime(year=today.year, month=origin_birthday.month, day=origin_birthday.day).date()
+            if today <= this_year_birthday <= end_date:
+                contacts.append(contact)
+    return contacts
